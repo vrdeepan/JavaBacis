7ae89b127b74d84c46482a0ab4b6874a374b5246
vrdeepan <vrdeepan@gmail.com>
2022-07-30 07:50:34 +0530

    Updates

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.print("Hi"); //This is print without newline
+        System.out.println(3+3);
+        /* Sample Multiline 
+        Comment */
+    }      
+}
---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name= "Deepan";
+        int age = 34;
+        System.out.println(name);
+        System.out.println("Age: " + age);
+        //Combining Two Strings
+        String firstname = "Deepan";
+        String lastname = "Kumar";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+        //Mathematical operations
+        int a = 1, b = 2, c = 3;
+        System.out.println(a + b + c);
+        //One Value to Multiple Variables
+        int x, y, z;
+        x = y = z = 10;
+        System.out.println(x);
+    }
+}
